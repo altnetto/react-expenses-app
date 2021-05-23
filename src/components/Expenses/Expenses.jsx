7ac5ccd320d4125c965a0e15fc167f50878cc9c0
altnetto <altnetto@gmail.com>
2021-05-23 10:05:41 -0300

    Filter is correctly working

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,20 +1,32 @@
 import "./Expenses.css";
+import React, { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from './ExpensesFilter';
 import Card from "../UI/Card";
 
 export default function Expenses(props) {
+  const [selectedYear, setSelectedYear] = useState('2021');
+  const filterChangeHandler = (selectedYear) => {
+    setSelectedYear(selectedYear.toString());
+  };
+  
   return (
-    <Card className="expenses">
-      {props.expenses.map((item) => {
-        return (
-          <ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-        );
-      })}
-    </Card>
+    <div>
+      <ExpensesFilter onFilterChange={ filterChangeHandler }/>
+      <Card className="expenses">
+        {props.expenses.map((item) => {
+          if (item.date.getFullYear().toString() === selectedYear) {
+            return (
+              <ExpenseItem
+                key={item.id}
+                title={item.title}
+                amount={item.amount}
+                date={item.date}
+              />
+            );
+          }
+        })}
+      </Card>
+    </div>
   );
 }
---
+++ src/components/Expenses/ExpensesFilter.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+
+  const [selectedYear, setSelectedYear] = useState('2021');
+
+  const selectYearHandler = (event) => {
+    props.onFilterChange(event.target.value.toString());
+    setSelectedYear(event.target.value.toString());
+  }
+  
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select onChange={ selectYearHandler } value={ selectedYear }>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
